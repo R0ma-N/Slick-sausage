f9a6ca246ea4794e3f3badecc1c37a635c3f9dd9
R0ma-N <x6wert@gmail.com>
2021-07-01 08:30:15 +0300

    #4

---
+++ Assets/Scripts/DeathPlane.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DeathPlane : MonoBehaviour
+{
+    Sausage sausage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.TryGetComponent<Sausage>(out sausage);
+        if (sausage)
+        {
+            sausage.isFailed = true;
+        }
+    }
+}
